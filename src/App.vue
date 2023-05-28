343eb45bf961c0b5971e489d7d4ecc87e93c67db
Ash Lawson <108193997+Ashyy-code@users.noreply.github.com>
2023-05-28 17:27:03 +0100

    Removed componentisation not neccessary

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,45 @@
 <template>
   <div class="set-wrap">
     <div class="hero-set">
-      <div v-for="hero in legion_agi" :key="hero.hero_name" class="hero">
-        <img v-if="!hero.original_img" :src="'/images/' + hero.hero_img" />
-        <img og v-if="hero.original_img" :src="'/images/' + hero.original_img" />
+      <div @mouseenter="showPP($event.target)" @mouseleave="hidePP($event.target)" v-for="hero in legion_agi" :key="hero.hero_name" class="hero">
+        
+        <img pp :src="'/images/' + hero.hero_img" />
+        <img  og v-if="hero.original_img" :src="'/images/' + hero.original_img" />
 
       </div>
     </div>
     <div class="hero-set">
-      <div v-for="hero in hellbourne_agi" :key="hero.hero_name" class="hero">
-        <img v-if="!hero.original_img" :src="'/images/' + hero.hero_img" />
+      <div @mouseenter="showPP($event.target)" @mouseleave="hidePP($event.target)" v-for="hero in hellbourne_agi" :key="hero.hero_name" class="hero">
+        
+        <img pp :src="'/images/' + hero.hero_img" />
         <img og v-if="hero.original_img" :src="'/images/' + hero.original_img" />
       </div>
     </div>
-    <div class="hero-set">
-      <div v-for="hero in legion_int" :key="hero.hero_name" class="hero">
-        <img v-if="!hero.original_img" :src="'/images/' + hero.hero_img" />
+    <div  class="hero-set">
+      <div @mouseenter="showPP($event.target)" @mouseleave="hidePP($event.target)" v-for="hero in legion_int" :key="hero.hero_name" class="hero">
+        
+        <img pp :src="'/images/' + hero.hero_img" />
         <img og v-if="hero.original_img" :src="'/images/' + hero.original_img" />
       </div>
     </div>
-    <div class="hero-set">
-      <div v-for="hero in hellbourne_int" :key="hero.hero_name" class="hero">
-        <img v-if="!hero.original_img" :src="'/images/' + hero.hero_img" />
+    <div   class="hero-set">
+      <div @mouseenter="showPP($event.target)" @mouseleave="hidePP($event.target)" v-for="hero in hellbourne_int" :key="hero.hero_name" class="hero">
+        
+        <img  pp :src="'/images/' + hero.hero_img" />
         <img og v-if="hero.original_img" :src="'/images/' + hero.original_img" />
       </div>
     </div>
-    <div class="hero-set">
-      <div v-for="hero in legion_str" :key="hero.hero_name" class="hero">
-        <img v-if="!hero.original_img" :src="'/images/' + hero.hero_img" />
+    <div  class="hero-set">
+      <div @mouseenter="showPP($event.target)" @mouseleave="hidePP($event.target)" v-for="hero in legion_str" :key="hero.hero_name" class="hero">
+        
+        <img  pp :src="'/images/' + hero.hero_img" />
         <img og v-if="hero.original_img" :src="'/images/' + hero.original_img" />
       </div>
     </div>
-    <div class="hero-set">
-      <div v-for="hero in hellbourne_str" :key="hero.hero_name" class="hero">
-        <img v-if="!hero.original_img" :src="'/images/' + hero.hero_img" />
+    <div  class="hero-set">
+      <div @mouseenter="showPP($event.target)" @mouseleave="hidePP($event.target)" v-for="hero in hellbourne_str" :key="hero.hero_name" class="hero">
+        
+        <img  pp :src="'/images/' + hero.hero_img" />
         <img og v-if="hero.original_img" :src="'/images/' + hero.original_img" />
       </div>
     </div>
@@ -61,6 +67,12 @@ export default {
         })
       );
     },
+    showPP(element){
+      element.querySelector('[og]')?.setAttribute('show','');
+    },
+    hidePP(element){
+      element.querySelector('[og]')?.removeAttribute('show');
+    }
   },
 
   data() {
@@ -98,15 +110,32 @@ body{
   
 
     .hero{
+      position:relative;
       img{
         height:65px;
         width:65px;
         object-fit:cover;
         opacity:.3;
+        cursor: pointer;
+
+
 
         &[og]{
           opacity:1;
           border:solid 1px gold;
+          position:absolute;
+          top:0;
+          left:0;
+          z-index: 9999;
+          transform:none;
+          transition:transform 200ms ease;
+
+          &[show]{
+            transform:translateX(85%) rotate(25deg) translateY(-25%);
+          }
+        }
+
+        &[pp]{
 
 
         }
